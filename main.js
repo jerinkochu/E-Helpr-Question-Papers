66955789a7040a43899e4e04dd581e1963456f2e
arunchamakkalayil <strangerthings369369@gmail.com>
2022-02-02 09:10:06 -0400

    Bcom session updated

---
+++ main.js
@@ -0,0 +1,55 @@
+function bcom() {
+
+    var x = document.getElementById("mySelect").value;
+
+    if(x==1){
+        document.getElementById("main").style.display="block";
+        document.getElementById('main').scrollIntoView(60);
+    }else{
+        document.getElementById("main").style.display="none";
+
+    }
+    if(x==2){
+        document.getElementById("main-2").style.display="block";
+        document.getElementById('main-2').scrollIntoView(60);
+    }else{
+        document.getElementById("main-2").style.display="none";
+
+    }
+    if(x==3){
+        document.getElementById("main-3").style.display="block";
+        document.getElementById('main-3').scrollIntoView(60);
+    }else{
+        document.getElementById("main-3").style.display="none";
+
+    }
+    if(x==4){
+        document.getElementById("main-4").style.display="block";
+        document.getElementById('main-4').scrollIntoView(60);
+    }else{
+        document.getElementById("main-4").style.display="none";
+
+    }
+    if(x==5){
+        document.getElementById("main-5").style.display="block";
+        document.getElementById('main-5').scrollIntoView(60);
+    }else{
+        document.getElementById("main-5").style.display="none";
+
+    }
+    
+    if(x==6){
+        document.getElementById("main-6").style.display="block";
+        document.getElementById('main-6').scrollIntoView(60);
+    }else{
+        document.getElementById("main-6").style.display="none";
+
+    }
+    
+    
+
+    
+
+
+}
+
