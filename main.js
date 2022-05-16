5570cf5cb0d46707cc1a4b6ad23590b3d40cca3d
arunchamakkalayil <strangerthings369369@gmail.com>
2022-05-16 12:56:52 -0400

    commit on 16

--- main.js
+++ main.js
@@ -1,90 +1,67 @@
 function finder() {
+  var x = document.getElementById("mySelect").value;
 
-    var x = document.getElementById("mySelect").value;
+  if (x == 1) {
+    document.getElementById("main").style.display = "block";
+    document.getElementById("main").scrollIntoView(60);
+  } else {
+    document.getElementById("main").style.display = "none";
+  }
+  if (x == 2) {
+    document.getElementById("main-2").style.display = "block";
+    document.getElementById("main-2").scrollIntoView(60);
+  } else {
+    document.getElementById("main-2").style.display = "none";
+  }
+  if (x == 3) {
+    document.getElementById("main-3").style.display = "block";
+    document.getElementById("main-3").scrollIntoView(60);
+  } else {
+    document.getElementById("main-3").style.display = "none";
+  }
+  if (x == 4) {
+    document.getElementById("main-4").style.display = "block";
+    document.getElementById("main-4").scrollIntoView(60);
+  } else {
+    document.getElementById("main-4").style.display = "none";
+  }
+  if (x == 5) {
+    document.getElementById("main-5").style.display = "block";
+    document.getElementById("main-5").scrollIntoView(60);
+  } else {
+    document.getElementById("main-5").style.display = "none";
+  }
 
-   
-
-    if(x==1){
-        document.getElementById("main").style.display="block";
-        document.getElementById('main').scrollIntoView(60);
-    }else{
-        document.getElementById("main").style.display="none";
-
-    }
-    if(x==2){
-        document.getElementById("main-2").style.display="block";
-        document.getElementById('main-2').scrollIntoView(60);
-    }else{
-        document.getElementById("main-2").style.display="none";
-
-    }
-    if(x==3){
-        document.getElementById("main-3").style.display="block";
-        document.getElementById('main-3').scrollIntoView(60);
-    }else{
-        document.getElementById("main-3").style.display="none";
-
-    }
-    if(x==4){
-        document.getElementById("main-4").style.display="block";
-        document.getElementById('main-4').scrollIntoView(60);
-    }else{
-        document.getElementById("main-4").style.display="none";
-
-    }
-    if(x==5){
-        document.getElementById("main-5").style.display="block";
-        document.getElementById('main-5').scrollIntoView(60);
-    }else{
-        document.getElementById("main-5").style.display="none";
-
-    }
-    
-    if(x==6){
-        document.getElementById("main-6").style.display="block";
-        document.getElementById('main-6').scrollIntoView(60);
-    }else{
-        document.getElementById("main-6").style.display="none";
-
-    }
-    if(x==7){
-        
-        document.getElementById("main-7").style.display="block";
-        document.getElementById('main-7').scrollIntoView(60);
-    }else{
-        document.getElementById("main-7").style.display="none";
-
-    }
-    if(x==8){
-        document.getElementById("main-8").style.display="block";
-        document.getElementById('main-8').scrollIntoView(60);
-    }else{
-        document.getElementById("main-8").style.display="none";
-
-    }
-    if(x==9){
-       
-        document.getElementById("main-9").style.display="block";
-        document.getElementById('main-9').scrollIntoView(60);
-    }else{
-        document.getElementById("main-9").style.display="none";
-
-    }
-    if(x==10){
-        document.getElementById("main-10").style.display="block";
-        document.getElementById('main-10').scrollIntoView(60);
-    }else{
-        document.getElementById("main-10").style.display="none";
-
-    }
-    
+  if (x == 6) {
+    document.getElementById("main-6").style.display = "block";
+    document.getElementById("main-6").scrollIntoView(60);
+  } else {
+    document.getElementById("main-6").style.display = "none";
+  }
+  if (x == 7) {
+    document.getElementById("main-7").style.display = "block";
+    document.getElementById("main-7").scrollIntoView(60);
+  } else {
+    document.getElementById("main-7").style.display = "none";
+  }
+  if (x == 8) {
+    document.getElementById("main-8").style.display = "block";
+    document.getElementById("main-8").scrollIntoView(60);
+  } else {
+    document.getElementById("main-8").style.display = "none";
+  }
+  if (x == 9) {
+    document.getElementById("main-9").style.display = "block";
+    document.getElementById("main-9").scrollIntoView(60);
+  } else {
+    document.getElementById("main-9").style.display = "none";
+  }
+  if (x == 10) {
+    document.getElementById("main-10").style.display = "block";
+    document.getElementById("main-10").scrollIntoView(60);
+  } else {
+    document.getElementById("main-10").style.display = "none";
+  }
 }
 
-window.addEventListener("load",finder);
- 
-
-
-
-
-
-  
+window.addEventListener("load", finder);
